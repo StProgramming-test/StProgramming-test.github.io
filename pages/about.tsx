74336a968a114f387c8aac0b36d25b11491621e0
tuna-tuna <bouningen13@gmail.com>
2022-04-22 13:00:45 +0900

    added about

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+                <meta name="description" content="About Me"/>
+            </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
